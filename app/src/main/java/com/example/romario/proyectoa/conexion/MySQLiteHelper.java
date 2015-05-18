bc720695123816662a021c00bb34900f6035520f
hoscanoa <hoscanoa@gmail.com>
2015-05-17 20:34:35 -0500

    daos por beans completos

--- app/src/main/java/com/example/romario/proyectoa/conexion/MySQLiteHelper.java
+++ app/src/main/java/com/example/romario/proyectoa/conexion/MySQLiteHelper.java
@@ -4,6 +4,10 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
 /**
  * Created by Hernan on 17/05/2015.
  */
@@ -13,9 +17,37 @@ public class MySQLiteHelper extends SQLiteOpenHelper {
         super(context, "ProyectoDAMI", factory, version);
     }
 
+    private String fileReader()
+    {
+        File archivo = null;
+        FileReader fr = null;
+        BufferedReader br = null;
+        String texto=null;
+        try { archivo = new File ("script.sqlite");
+            fr = new FileReader (archivo);
+            br = new BufferedReader(fr);
+            texto="";
+            String linea;
+            while((linea=br.readLine())!=null)
+                texto+=linea;
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }finally{
+            try{
+                if( null != fr ){
+                    fr.close();
+                }
+            }catch (Exception e2){
+                e2.printStackTrace();
+            }
+        }
+        return texto;
+    }
+
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("");
+        db.execSQL(fileReader());
     }
 
     @Override
