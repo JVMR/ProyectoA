cf2258458321413d00738f96113a06d5171592d5
hoscanoa <hoscanoa@gmail.com>
2015-05-19 09:46:22 -0500

    comunicacion a activities

--- app/src/main/java/com/example/romario/proyectoa/OpcionesActivity.java
+++ app/src/main/java/com/example/romario/proyectoa/OpcionesActivity.java
@@ -1,17 +1,36 @@
 package com.example.romario.proyectoa;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.romario.proyectoa.beans.Profesor;
 
 
-public class OpcionesActivity extends ActionBarActivity {
+public class OpcionesActivity extends ActionBarActivity implements View.OnClickListener {
+
+    Button btnExportar, btnImportar, btnRegistroNotas;
+    Profesor profesor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_opciones);
+
+        Intent rec = getIntent();
+        profesor = (Profesor)rec.getSerializableExtra("profesor");
+
+        btnExportar=(Button)findViewById(R.id.btnExportar);
+        btnExportar.setOnClickListener(this);
+        btnImportar=(Button)findViewById(R.id.btnImportar);
+        btnImportar.setOnClickListener(this);
+        btnRegistroNotas=(Button)findViewById(R.id.btnRegistroNotas);
+        btnRegistroNotas.setOnClickListener(this);
+
     }
 
 
@@ -35,5 +54,24 @@ public class OpcionesActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent ir;
+        if(v==btnExportar)
+        {
+            ir= new Intent(this,ExportarActivity.class);
+        }
+        else if(v==btnImportar)
+        {
+            ir= new Intent(this,ImportarActivity.class);
+        }
+        else
+        {
+            ir= new Intent(this,RegistrarNotasActivity.class);
+        }
+        ir.putExtra("profesor",profesor);
+        startActivity(ir);
     }
 }
